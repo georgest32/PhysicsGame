0fbd4247d5e881f7676ac01023b536dd94a09d44
George Still <georgest32@gmail.com>
2020-12-08 11:48:59 -0500

    added camera lerp on play

--- MermaidPhysicsGame/Assets/Scripts/MenuController.cs
+++ MermaidPhysicsGame/Assets/Scripts/MenuController.cs
@@ -5,22 +5,45 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     public GameObject mainCam;
+    public Transform player;
+    Vector3 camStartPos;
+    public Transform trans;
+    public float CamMoveSpeed = 5f;
+    private bool startingGame;
+    private float camTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camStartPos = new Vector3(100f, -10.8f + 3.1f, -632 + -15.5f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startingGame)
+        {
+            trans.LookAt(player.position);
+
+            trans.position = Vector3.Lerp(trans.position, camStartPos, CamMoveSpeed * Time.deltaTime);
+
+            if (Time.time - camTime > 3)
+            {
+                startingGame = false;
+                mainCam.GetComponent<Orbital>().enabled = true;
+                player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                GameObject.FindGameObjectWithTag("CandleFlame").GetComponent<Light>().enabled = true;
+            }
+        }
     }
 
-    public void Play()
+        public void Play()
     {
         GameObject.FindGameObjectWithTag("Menu").SetActive(false);
-        mainCam.GetComponent<Orbital>().enabled = true;
+
+        camTime = Time.time;
+
+        startingGame = true;
     }
 }
