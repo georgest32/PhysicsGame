14e2accc81a974c56e4f4894a5196ad4b399d498
Yanxi <w1y2f3@126.com>
2020-12-09 10:51:40 -0800

    Add About menu

---
+++ MermaidPhysicsGame/Assets/Scripts/AboutTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AboutTrigger : MonoBehaviour
+{
+    public GameObject about;
+  public void triggerAbout()
+    {
+        about.SetActive(true);
+    }
+}
